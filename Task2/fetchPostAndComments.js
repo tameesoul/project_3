9e317af0f225850dd12c546a43febd88fa818338
tameesoul <tamerghazy@std.mans.edu.eg>
2025-07-23 13:42:04 +0300

    add comment for task2

--- Task2/fetchPostAndComments.js
+++ Task2/fetchPostAndComments.js
@@ -4,18 +4,22 @@
 async function fetchPostAndComments() {
     const postUrl = 'https://jsonplaceholder.typicode.com/posts/50';
     const commentsUrl = 'https://jsonplaceholder.typicode.com/posts/50/comments';
-  
+
+      // Fetch the post
     const postResponse = await fetch(postUrl);
     const post = await postResponse.json();
   
+      // Fetch comments related to that post
     const commentsResponse = await fetch(commentsUrl);
     const comments = await commentsResponse.json();
   
+      // Combine post and its comments in one object
     const merged = {
-      ...post,
-      comments: comments
+      ...post, // Spread all post fields
+      comments: comments // Add comments as a new field
     };
   
+      // Log the combined result
     console.log("Merged Post + Comments:", merged);
   }
   
